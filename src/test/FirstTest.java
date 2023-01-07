538dd1e9dbd927ef112a70fe06c423b45b3c8b36
sowa <doroshenko.tetyana@gmail.com>
2023-01-08 00:18:05 +0100

    init

---
+++ src/test/FirstTest.java
@@ -0,0 +1,17 @@
+package src.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void testAdd() {
+
+    }
+
+    @Test
+    public void testEdit() {
+        Assert.assertEquals(1, 0);
+    }
+}
