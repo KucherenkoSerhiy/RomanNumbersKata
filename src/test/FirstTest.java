8e49ed3edde8b5d9ab0e1120bd6026457f2bf841
sowa <doroshenko.tetyana@gmail.com>
2023-01-08 00:51:13 +0100

    red

--- src/test/FirstTest.java
+++ src/test/FirstTest.java
@@ -2,16 +2,20 @@ package src.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import src.main.Application;
 
 public class FirstTest {
 
     @Test
     public void testAdd() {
+        //arrange
 
+
+        //act
+        String actualResponse = Application.toRomanNumber(1);
+
+        //assert
+        Assert.assertEquals("I", actualResponse);
     }
 
-    @Test
-    public void testEdit() {
-        Assert.assertEquals(1, 0);
-    }
 }
